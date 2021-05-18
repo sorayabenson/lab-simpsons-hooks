3e526490a39c1190ee5346628833db4380ce3866
Soraya Benson <soraya.benson@gmail.com>
2021-05-18 15:37:46 -0700

    styling tweaked

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -5,6 +5,7 @@ import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import userEvent from '@testing-library/user-event';
 
+
 const server = setupServer(
   rest.get(
     'https://thesimpsonsquoteapi.glitch.me/quotes',
